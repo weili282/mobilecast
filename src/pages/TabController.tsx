9f15311ba5b2b9eeaf35e6f779da15f31ffedccf
wei.2.li@sprint.com-at-430241707116 <wei.2.li@sprint.com>
2022-07-22 13:26:03 -0400

    up

--- src/pages/TabController.tsx
+++ src/pages/TabController.tsx
@@ -85,7 +85,7 @@ return(
       </IonToolbar>
     </IonHeader>
 
-    <IonReactRouter basename='/pwa'>
+    <IonReactRouter >
       <IonTabs >
         <IonRouterOutlet>
         <Route exact path={props.match.url} component={Tab1} />
